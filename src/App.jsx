b23fd9f6bc841e7a04238d4784205e075c368e82
Vina Quiambao <mariavinaq@gmail.com>
2024-10-17 19:35:19 -0400

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Feed from "./components/Feed/Feed";
 import SuggestionForm from "./components/SuggestionForm/SuggestionForm";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Feed />}></Route>
           <Route path='/suggestion' element={<SuggestionForm />}></Route>
