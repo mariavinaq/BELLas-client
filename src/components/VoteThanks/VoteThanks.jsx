458a348bf53f7d2b900f304048f709aa3e2d65f1
Sabrina B <sabrinaberih@gmail.com>
2024-10-18 09:05:15 -0400

    Add thanks for voting page

--- src/components/VoteThanks/VoteThanks.jsx
+++ src/components/VoteThanks/VoteThanks.jsx
@@ -1,5 +1,5 @@
 import "./VoteThanks.scss";
-import thanksVoting from "../../assets/images/thanksVoting";
+import thanksVoting from "../../assets/images/thanksVoting.png";
 import { useNavigate } from "react-router-dom";
 
 function VoteThanks() {
@@ -10,7 +10,37 @@ function VoteThanks() {
     window.location.reload();
   }
 
-  return <div>Vote Thanks</div>;
+  function handleOnClickSuggestion() {
+    navigate("/submit");
+    window.location.reload();
+  }
+
+  return (
+    <>
+      <div className="vote-thanks">
+        <img
+          className="vote-thanks__image"
+          src={thanksVoting}
+          alt="Man holding heart"
+        />
+        <p className="vote-thanks__text">
+          Want to make a change in your community?
+        </p>
+        <button
+          className="vote-thanks__button vote-thanks__button--go-suggestion"
+          onClick={handleOnClickSuggestion}
+        >
+          <p className="vote-thanks__button-text">Submit your suggestion</p>
+        </button>
+        <button
+          className="vote-thanks__button vote-thanks__button--go-home"
+          onClick={handleOnClick}
+        >
+          <p className="vote-thanks__button-text">Back to Home</p>
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default VoteThanks;
