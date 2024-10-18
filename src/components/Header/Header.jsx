e1237bc76a88dcb0115ab8b0d047049933e79150
Sabrina B <sabrinaberih@gmail.com>
2024-10-18 11:30:10 -0400

    commit changes before merge

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,24 @@
 import { Link } from "react-router-dom";
-import chevron from "../../assets/images/chevron-white.svg"
+import chevron from "../../assets/images/chevron-white.svg";
+import logo from "../../assets/images/bell_white.svg";
 import "./Header.scss";
 
 function Header() {
-    return (
-        <div className='header'>
-            <a className='header__initiatives' href='https://www.bell.ca/bell-for-better' target='_blank'>
-                See our current initiatives
-                <img className='header__chevron' src={chevron} />
-            </a>
-        </div>
-    );
+  return (
+    <div className="header">
+      <Link to="/" className="header__logo-link">
+        <img className="header__logo" src={logo} />
+      </Link>
+      <a
+        className="header__initiatives"
+        href="https://www.bell.ca/bell-for-better"
+        target="_blank"
+      >
+        See our current initiatives
+        <img className="header__chevron" src={chevron} />
+      </a>
+    </div>
+  );
 }
 
 export default Header;
