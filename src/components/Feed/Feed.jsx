a68e310fcd807d5a72377cf79885f2bf2b7b5da2
Vina Quiambao <mariavinaq@gmail.com>
2024-10-17 21:05:46 -0400

    Initial styles

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -3,9 +3,11 @@ import './Feed.scss';
 
 function Feed () {
     return (
-        <div>
-            <div>
-                <img src={logo} />
+        <div className='feed'>
+            <div className='feed__hero'>
+                <img className='feed__logo' src={logo} />
+                <h1 className='feed__title'>Vote for change in your community</h1>
+                <p className='feed__blurb'>Bell is committed to contributing to a better world, community, and workplace. Join us in this mission by voicing your opinion and voting for the change you want to see most in your community. We’ll then partner with a community organization that can make the idea with the most votes a reality.</p>
             </div>
         </div>
     );
