5082f4995c8f9975febfcde383d70387c82c8be1
Vina Quiambao <mariavinaq@gmail.com>
2024-10-18 04:22:39 -0400

    Add link to Bell in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+import chevron from "../../assets/images/chevron-white.svg"
 import "./Header.scss";
 
 function Header() {
-  return <div className='header'></div>;
+    return (
+        <div className='header'>
+            <a className='header__initiatives' href='https://www.bell.ca/bell-for-better' target='_blank'>
+                See our current initiatives
+                <img className='header__chevron' src={chevron} />
+            </a>
+        </div>
+    );
 }
 
 export default Header;
