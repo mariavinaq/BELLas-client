4823b214a4b9c91ec022ff1a5e21c58cd400c445
Vina Quiambao <mariavinaq@gmail.com>
2024-10-17 23:14:53 -0400

    Save before pull

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,8 +1,31 @@
+import { useState } from 'react';
 import logo from '../../assets/images/Bell_logo.svg'
 import SuggestionOption from '../SuggestionOption/SuggestionOption';
 import './Feed.scss';
 
 function Feed() {
+    const [selectedTop, setSelectedTop] = useState(true);
+    const [selectedNew, setSelectedNew] = useState(false);
+    const [selectedAll, setSelectedAll] = useState(false);
+
+    const handleTop = () => {
+        setSelectedTop(true);
+        setSelectedNew(false);
+        setSelectedAll(false);
+    };
+
+    const handleNew = () => {
+        setSelectedTop(false);
+        setSelectedNew(true);
+        setSelectedAll(false);
+    };
+
+    const handleAll = () => {
+        setSelectedTop(false);
+        setSelectedNew(false);
+        setSelectedAll(true);
+    };
+
     return (
         <div className='feed'>
             <div className='feed__hero'>
@@ -11,9 +34,9 @@ function Feed() {
                 <p className='feed__blurb'>Bell is committed to contributing to a better world, community, and workplace. Join us in this mission by voicing your opinion and voting for the change you want to see most in your community. We’ll then partner with a community organization that can make the idea with the most votes a reality.</p>
             </div>
             <div className='feed__filters'>
-                <button className='feed__button'>Top</button>
-                <button className='feed__button'>New</button>
-                <button className='feed__button'>All</button>
+                <button className={`feed__button${selectedTop}`} onClick={handleTop}>Top</button>
+                <button className={`feed__button${selectedNew}`} onClick={handleNew}>New</button>
+                <button className={`feed__button${selectedAll}`} onClick={handleAll}>All</button>
             </div>
             <div className='feed__main'>
                 <SuggestionOption />
